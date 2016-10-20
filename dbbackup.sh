bdc4c1af1cac27830f8d17c5cd2a04a19d6e7d4b
jayakrishnanjr <jrjayakrishnan6@gmail.com>
2016-10-20 07:04:02 +0530

    mydbbackupThu Oct 20 07:04:02 IST 2016

--- dbbackup.sh
+++ dbbackup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-cd /home/jk/Desktop/backup-of-db-using-shellscript-and-crone
+cd /home/jk/Desktop/backup-of-db-using-shellscript-and-cron
 mysqldump -u root -proot jk --ignore-table=jk.ignoretable > backupdatas.sql
 git add -A
 git commit -m "mydbbackup`date`"
